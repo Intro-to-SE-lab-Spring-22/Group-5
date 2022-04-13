74265c8d65741d519c99fda08fc9af5bef71e38b
bananaox <80545648+SovaCoda@users.noreply.github.com>
2022-04-12 19:27:31 -0500

    added requirements

--- main/login/tests.py
+++ main/login/tests.py
@@ -23,10 +23,28 @@ class PostAndLikeTestCase(TestCase):
 class CreateAccountTestCase(TestCase):
     def setUp(self):
         User.objects.create(username='user', password="password", email='email@email.com')
+        User.objects.create(username='user@$#@OIO I$#@NTOINF?>><Z<X<C>XZ><<<F<MDVMMCVMNCXJJSA', password="password", email='email@email.com')
 
     def testUserCreation(self):
+        good = User.objects.get(username='user')
+        self.assertIsNotNone(good)
+
+    def testUserCreation(self):
+        bad = User.objects.get(username='user@$#@OIO I$#@NTOINF?>><Z<X<C>XZ><<<F<MDVMMCVMNCXJJSA')
+        self.assertEquals(bad.username, 'user@$#@OIO I$#@NTOINF?>><Z<X<C>XZ><<<F<MDVMMCVMNCXJJSA')
+
+
+class LoginAccountTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username='user', password="password")
+
+    def testUserLogin(self):
         me = User.objects.get(username='user')
-        me.check()
+        raw_password='password'
+        me.check_password(self)
+        if me.is_authenticated:
+            return True
+
 
 class LoginAccountTestCase(TestCase):
     def setUp(self):
